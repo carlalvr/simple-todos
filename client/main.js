bc5eb449bb99f199bc338fc919d7273cf5939fc7
carlalvr <carlalarvor@gmail.com>
2017-02-26 19:04:12 +0100

    main.js

--- client/main.js
+++ client/main.js
@@ -1,13 +1,1 @@
-import { Template } from 'meteor/templating';
- 
-import './body.html';
- 
-Template.body.helpers({
-  tasks: [
-    { text: 'This is task 1' },
-    { text: 'This is task 2' },
-    { text: 'This is task 3' },
-  ],
-});
-
 import '../imports/ui/body.js';
